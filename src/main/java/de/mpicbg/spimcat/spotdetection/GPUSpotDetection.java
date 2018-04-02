f25ab80cfd0d0ebceae545ad2f5d1231a8707207
haesleinhuepf <rhaase@mpi-cbg.de>
2018-04-02 11:10:43 +0200

    refactoring and opening the API

--- src/main/java/de/mpicbg/spimcat/spotdetection/GPUSpotDetection.java
+++ src/main/java/de/mpicbg/spimcat/spotdetection/GPUSpotDetection.java
@@ -13,56 +13,52 @@ import ij.plugin.RGBStackMerge;
 import java.io.IOException;
 
 public class GPUSpotDetection {
-    private static float samplingFactorX = 0.5f;
-    private static float samplingFactorY = 0.5f;
-    private static float samplingFactorZ = 1;
+    private float samplingFactorX = 0.5f;
+    private float samplingFactorY = 0.5f;
+    private float samplingFactorZ = 1;
     private static float threshold = 400;
 
     private static boolean cropPartForDebugging = true;
 
-    private static ClearCLIJ clij;
-    private static ImagePlus imp;
+    private ClearCLIJ clij;
+    //private static ImagePlus imp;
 
-    public static void main (String... args) throws IOException {
-        new ImageJ();
-        clij = new ClearCLIJ("HD");
-        ElapsedTime.sStandardOutput = true;
+    private ClearCLImage input;
+    private ClearCLImage output;
 
-        String file = "";
-        if (System.getProperty("os.name").startsWith("Windows")) {
-            file = "C:\\structure\\data\\Uncalibrated.tif";
-        } else {
-            file = "/home/rhaase/data/Uncalibrated.tif";
-        }
+    public GPUSpotDetection(ClearCLIJ clij, ClearCLImage input, ClearCLImage output, float threshold) {
+      this.clij = clij;
+      this.output = output;
+      this.input = input;
+      this.threshold = threshold;
 
-        imp = IJ.openImage(file);
-        imp.show();
+      samplingFactorX = (float)output.getWidth() / input.getWidth();
+      samplingFactorY = (float)output.getHeight() / input.getHeight();
+      samplingFactorZ = (float)output.getDepth() / input.getDepth();
 
-        for (int i = 0; i < 10; i++) {
-            ElapsedTime.measure("the whole thing ", () -> {
-                exec();
-            });
-        }
+
     }
 
-    private static void exec() {
+    public void exec() {
 
-        ClearCLImage clImp = clij.converter(imp).getClearCLImage();
-        ClearCLImage clInput = clImp;
-        long[] targetDimensions;
-        if (cropPartForDebugging) {
+        //ClearCLImage clImp = clij.converter(imp).getClearCLImage();
+        //ClearCLImage input = clImp;
+        long[] targetDimensions = new long[]{output.getWidth(), output.getHeight(), output.getDepth()};
+        /*if (cropPartForDebugging) {
             targetDimensions = Arrays.elementwiseMultiplVectors(clImp.getDimensions(), new float[]{0.5f, 0.5f, 1});
             clInput = clij.createCLImage(targetDimensions, clImp.getChannelDataType());
             Kernels.crop(clij, clImp, clInput, 256, 0, 0);
-        }
+        }*/
 
-        targetDimensions = Arrays.elementwiseMultiplVectors(clInput.getDimensions(), new float[]{samplingFactorX, samplingFactorY, samplingFactorZ});
-        ClearCLImage flip = clij.createCLImage(targetDimensions, clInput.getChannelDataType());
-        ClearCLImage flop = clij.createCLImage(targetDimensions, clInput.getChannelDataType());
-        ClearCLImage flap = clij.createCLImage(targetDimensions, clInput.getChannelDataType());
+        //targetDimensions = Arrays.elementwiseMultiplVectors(clInput.getDimensions(), new float[]{samplingFactorX, samplingFactorY, samplingFactorZ});
+        ClearCLImage flip = clij.createCLImage(targetDimensions, input.getChannelDataType());
+        ClearCLImage flop = output; //clij.createCLImage(targetDimensions, clInput.getChannelDataType());
+        ClearCLImage flap = clij.createCLImage(targetDimensions, input.getChannelDataType());
 
-        Kernels.downsample(clij, clInput, flop, samplingFactorX, samplingFactorY, samplingFactorZ);
+        System.out.println("factors " + samplingFactorX + "/" + samplingFactorY + "/" + samplingFactorZ);
+        Kernels.downsample(clij, input, flop, samplingFactorX, samplingFactorY, samplingFactorZ);
         clij.show(flop, "downsampled");
+
         ImagePlus downsampledImp = IJ.getImage();
 
 
@@ -78,8 +74,9 @@ public class GPUSpotDetection {
 
 
         // Spot detection
-        Kernels.differenceOfGaussian(clij, flop, flip, 6, 1.1f, 5f);
-        //clij.show(flip, "dog");
+        Kernels.differenceOfGaussian(clij, flop, flip, 6, 1.5f, 3f);
+        clij.show(flip, "dog");
+        ImagePlus dogImp = IJ.getImage();
 
         Kernels.detectMaxima(clij, flip, flop, 3);
         //clij.show(flop, "detected maxima");
@@ -93,9 +90,11 @@ public class GPUSpotDetection {
 
         ImagePlus spotsImp = IJ.getImage();
 
-        ImagePlus merged = RGBStackMerge.mergeChannels(new ImagePlus[]{downsampledImp, spotsImp}, true);
+        ImagePlus merged = RGBStackMerge.mergeChannels(new ImagePlus[]{downsampledImp, spotsImp, dogImp}, true);
         merged.show();
 
+        flip.close();
+        flap.close();
         System.out.print("Bye");
     }
 }
---
+++ src/main/java/de/mpicbg/spimcat/spotdetection/demo/GPUSpotDetectionDemo.java
@@ -0,0 +1,49 @@
+package de.mpicbg.spimcat.spotdetection.demo;
+
+import clearcl.ClearCLImage;
+import clearcl.imagej.ClearCLIJ;
+import clearcl.util.ElapsedTime;
+import de.mpicbg.spimcat.spotdetection.GPUSpotDetection;
+import ij.IJ;
+import ij.ImageJ;
+import ij.ImagePlus;
+
+import java.io.IOException;
+
+/**
+ * Author: Robert Haase (http://haesleinhuepf.net) at MPI CBG (http://mpi-cbg.de)
+ * April 2018
+ */
+public class GPUSpotDetectionDemo
+{
+
+  public static void main (String... args) throws IOException
+  {
+    final float threshold = 120;
+
+    new ImageJ();
+    ClearCLIJ clij = new ClearCLIJ("HD");
+    ElapsedTime.sStandardOutput = true;
+
+    String file = "";
+    if (System.getProperty("os.name").startsWith("Windows")) {
+      file = "C:\\structure\\data\\Uncalibrated.tif";
+    } else {
+      file = "/home/rhaase/data/Uncalibrated.tif";
+    }
+
+    ImagePlus imp = IJ.openImage(file);
+    imp.show();
+
+    //for (int i = 0; i < 10; i++) {
+      ElapsedTime.measure("the whole thing ", () -> {
+
+        ClearCLImage input = clij.converter(imp).getClearCLImage();
+        ClearCLImage output = clij.createCLImage(new long[]{input.getWidth()/2, input.getHeight()/2, input.getDepth()}, input.getChannelDataType());
+
+        new GPUSpotDetection(clij, input, output, threshold).exec();
+      });
+    //}
+  }
+
+}
